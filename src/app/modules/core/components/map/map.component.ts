e47ed4db0d5e1d0116889815bae844cb68545d7a
amedenblik <amedenblik@usgs.gov>
2020-09-15 17:09:06 -0400

    limit Trace to visible layers

--- src/app/modules/core/components/map/map.component.ts
+++ src/app/modules/core/components/map/map.component.ts
@@ -242,11 +242,15 @@ export class MapComponent extends deepCopy implements OnInit {
                         this.sm('Error occurred, check console');
                     }
                     if (results && results.features.length > 0) {
-                        this.MapService.Trace(results).subscribe((data => {
-                            console.log(data);
-                            const layer = L.geoJSON(data);
-                            this.selectedLayers.addLayer(layer);
-                        }));
+                        if (this.MapService.isLayerVisible(key)) {
+                          this.sm('Querying Trace, please wait...', 'wait', '', 60000); // This isn't doing anything
+                          this.MapService.Trace(results).subscribe((data => {
+                              console.log("Output Geojson: " + data);
+                              const layer = L.geoJSON(data);
+                              this.selectedLayers.addLayer(layer);
+                          }));
+                        }
+                        
 
                         results.features.forEach(feat => {
                             let popupcontent = '<div class="popup-header"><b>' + key + ':</b></div><br>';
@@ -275,11 +279,14 @@ export class MapComponent extends deepCopy implements OnInit {
                         this.sm('Error occurred, check console');
                     }
                     if (results && results.features.length > 0) {
-                        this.MapService.Trace(results).subscribe((data => {
-                            console.log(data);
-                            const layer = L.geoJSON(data);
-                            this.selectedLayers.addLayer(layer);
-                        }));
+                        if (this.MapService.isLayerVisible(key)) {
+                          this.sm('Querying Trace, please wait...', 'wait', '', 60000); // This isn't doing anything
+                          this.MapService.Trace(results).subscribe((data => {
+                              console.log("Output Geojson: " + data);
+                              const layer = L.geoJSON(data);
+                              this.selectedLayers.addLayer(layer);
+                          }));
+                        }
                         results.features.forEach(feat => {
                             let popupcontent = '<div class="popup-header"><b>' + key + ':</b></div><br>';
                             let date = feat.properties.STARTMONTH + '/' + feat.properties.STARTDAY + '/' +
